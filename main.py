1e32b215f69a96006f3eddf5c2b5001e8ec564af
Paul Alfredo <155785978+paulafredo@users.noreply.github.com>
2025-05-15 05:46:46 +0200

    add typing

--- main.py
+++ main.py
@@ -6,24 +6,19 @@ from flask import Flask
 import threading
 from utils import check_ban
 
-
 app = Flask(__name__)
-
 
 load_dotenv()
 APPLICATION_ID = os.getenv("APPLICATION_ID")
 TOKEN = os.getenv("TOKEN")
 
-
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-
-
 DEFAULT_LANG = "en"
 user_languages = {}
- 
+
 nomBot = "None"
 
 @app.route('/')
@@ -31,13 +26,10 @@ def home():
     global nomBot
     return f"Bot {nomBot} is working"
 
-
 def run_flask():
     app.run(host='0.0.0.0', port=10000)
 
-
 threading.Thread(target=run_flask).start()
-
 
 @bot.event
 async def on_ready():
@@ -50,23 +42,23 @@ async def show_guilds(ctx):
     guild_names = [f"{i+1}. {guild.name}" for i, guild in enumerate(bot.guilds)]
     guild_list = "\n".join(guild_names)
     await ctx.send(f"Le bot est dans les guilds suivantes :\n{guild_list}")
-    
+
 @bot.command(name="lang")
-async def change_language(ctx,lang_code : str):
+async def change_language(ctx, lang_code: str):
     lang_code = lang_code.lower()
-    if lang_code not in ["en","fr"]:
+    if lang_code not in ["en", "fr"]:
         await ctx.send("❌ Invalid language. Available: `en`, `fr`")
-        return 
-    
-    user_languages[ctx.author.id]=lang_code
-    message="✅ Language set to English." if lang_code == 'en' else "✅ Langue définie sur le français."
+        return
+
+    user_languages[ctx.author.id] = lang_code
+    message = "✅ Language set to English." if lang_code == 'en' else "✅ Langue définie sur le français."
     await ctx.send(f"{ctx.author.mention} {message}")
-    
-@bot.command(name="ID")  
+
+@bot.command(name="ID")
 async def check_ban_command(ctx):
     content = ctx.message.content
     user_id = content[3:].strip()
-    lang = user_languages.get(ctx.author.id, "en")  
+    lang = user_languages.get(ctx.author.id, "en")
 
     print(f"Commande fait par {ctx.author} (lang={lang})")
 
@@ -78,60 +70,61 @@ async def check_ban_command(ctx):
         await ctx.send(message[lang])
         return
 
-    try:
-        ban_status = await check_ban(user_id)
-    except Exception as e:
-        await ctx.send(f"{ctx.author.mention} ⚠️ Error:\n```{str(e)}```")
-        return
+    async with ctx.typing():
+        try:
+            ban_status = await check_ban(user_id)
+        except Exception as e:
+            await ctx.send(f"{ctx.author.mention} ⚠️ Error:\n```{str(e)}```")
+            return
 
-    if ban_status is None:
-        message = {
-            "en": f"{ctx.author.mention} ❌ **Could not get information. Please try again later.**",
-            "fr": f"{ctx.author.mention} ❌ **Impossible d'obtenir les informations.**\nVeuillez réessayer plus tard."
-        }
-        await ctx.send(message[lang])
-        return
+        if ban_status is None:
+            message = {
+                "en": f"{ctx.author.mention} ❌ **Could not get information. Please try again later.**",
+                "fr": f"{ctx.author.mention} ❌ **Impossible d'obtenir les informations.**\nVeuillez réessayer plus tard."
+            }
+            await ctx.send(message[lang])
+            return
 
-    is_banned = int(ban_status.get("is_banned", 0))
-    period = ban_status.get("period", "N/A")
-    nickname = ban_status.get("nickname","NA")
-    region = ban_status.get("region","N/A")
-    id_str = f"`{user_id}`"
+        is_banned = int(ban_status.get("is_banned", 0))
+        period = ban_status.get("period", "N/A")
+        nickname = ban_status.get("nickname", "NA")
+        region = ban_status.get("region", "N/A")
+        id_str = f"`{user_id}`"
 
-    if isinstance(period, int):
-        period_str = f"more than {period} months" if lang == "en" else f"plus de {period} mois"
-    else:
-        period_str = "unavailable" if lang == "en" else "indisponible"
+        if isinstance(period, int):
+            period_str = f"more than {period} months" if lang == "en" else f"plus de {period} mois"
+        else:
+            period_str = "unavailable" if lang == "en" else "indisponible"
 
-    embed = discord.Embed(
-        color=0xFF0000 if is_banned else 0x00FF00,
-        timestamp=ctx.message.created_at
-    )
-
-    if is_banned:
-        embed.title = "**▌ Banned Account 🛑 **" if lang == "en" else "**▌ Compte banni 🛑 **"
-        embed.description = (
-            f"**• {'Reason' if lang == 'en' else 'Raison'} :** "
-            f"{'This account was confirmed for using cheats.' if lang == 'en' else 'Ce compte a été confirmé comme utilisant des hacks.'}\n"
-            f"**• {'Suspension duration' if lang == 'en' else 'Durée de la suspension'} :** {period_str}\n"
-            f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** `{nickname}`\n"
-            f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** `{id_str}`\n"
-            f"**• {'Region' if lang == 'en' else 'Région'} :** `{region}`"
+        embed = discord.Embed(
+            color=0xFF0000 if is_banned else 0x00FF00,
+            timestamp=ctx.message.created_at
         )
-        embed.set_image(url="https://i.ibb.co/tDnbYrK/standard-1.gif")
-    else:
-        embed.title = "**▌ Clean Account ✅ **" if lang == "en" else "**▌ Compte non banni ✅ **"
-        embed.description = (
-            f"**• {'Status' if lang == 'en' else 'Statut'} :** "
-            f"{'No sufficient evidence of cheat usage on this account.' if lang == 'en' else 'Aucune preuve suffisante pour confirmer l’utilisation de hacks sur ce compte.'}\n"
-            f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** `{nickname}`\n"
-            f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** `{id_str}`\n"
-            f"**• {'Region' if lang == 'en' else 'Région'} :** `{region}`"
-        )
-        embed.set_image(url="https://i.ibb.co/CshJSf8/standard-2.gif")
-    embed.set_thumbnail(url=ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url)
-    embed.set_footer(text="📌  Garena Free Fire")
-    await ctx.send(f"{ctx.author.mention}", embed=embed)
 
+        if is_banned:
+            embed.title = "**▌ Banned Account 🛑 **" if lang == "en" else "**▌ Compte banni 🛑 **"
+            embed.description = (
+                f"**• {'Reason' if lang == 'en' else 'Raison'} :** "
+                f"{'This account was confirmed for using cheats.' if lang == 'en' else 'Ce compte a été confirmé comme utilisant des hacks.'}\n"
+                f"**• {'Suspension duration' if lang == 'en' else 'Durée de la suspension'} :** {period_str}\n"
+                f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** `{nickname}`\n"
+                f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** `{id_str}`\n"
+                f"**• {'Region' if lang == 'en' else 'Région'} :** `{region}`"
+            )
+            embed.set_image(url="https://i.ibb.co/tDnbYrK/standard-1.gif")
+        else:
+            embed.title = "**▌ Clean Account ✅ **" if lang == "en" else "**▌ Compte non banni ✅ **"
+            embed.description = (
+                f"**• {'Status' if lang == 'en' else 'Statut'} :** "
+                f"{'No sufficient evidence of cheat usage on this account.' if lang == 'en' else 'Aucune preuve suffisante pour confirmer l’utilisation de hacks sur ce compte.'}\n"
+                f"**• {'Nickname' if lang == 'en' else 'Pseudo'} :** `{nickname}`\n"
+                f"**• {'Player ID' if lang == 'en' else 'ID du joueur'} :** `{id_str}`\n"
+                f"**• {'Region' if lang == 'en' else 'Région'} :** `{region}`"
+            )
+            embed.set_image(url="https://i.ibb.co/CshJSf8/standard-2.gif")
+
+        embed.set_thumbnail(url=ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url)
+        embed.set_footer(text="📌  Garena Free Fire")
+        await ctx.send(f"{ctx.author.mention}", embed=embed)
 
 bot.run(TOKEN)
