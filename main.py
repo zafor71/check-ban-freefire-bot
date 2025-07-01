9b7df94a8d96703cf446eb61dc4e11385e983781
zafor71 <idxbdff@gmail.com>
2025-07-01 12:18:26 +0600

    Update main.py

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ async def check_ban_command(ctx):
             embed.set_image(url="attachment://notbanned.gif")
 
         embed.set_thumbnail(url=ctx.author.avatar.url if ctx.author.avatar else ctx.author.default_avatar.url)
-        embed.set_footer(text="DEVELOPED BY THUG•")
+        embed.set_footer(text="DEVELOPED BY TANVIR•")
         await ctx.send(f"{ctx.author.mention}", embed=embed ,file=file)
 
 bot.run(TOKEN)
